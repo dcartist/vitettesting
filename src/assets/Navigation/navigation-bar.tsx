546e6b9989a756314dd1096ec8753bcb10b0ea27
dcartist <pbannerman13@gmail.com>
2022-11-01 23:44:32 -0400

    adding menu

---
+++ src/assets/Navigation/navigation-bar.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+
+export const NavBar = () => {
+  const [showNavText, setShowNavText] = useState(false);
+
+  return (
+    <MDBNavbar expand="lg" light bgColor="light">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="#">FlickPix</MDBNavbarBrand>
+        <MDBNavbarToggler
+          type="button"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setShowNavText(!showNavText)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNavText}>
+          <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="#">
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#">Features</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#">Pricing</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+          <span className="navbar-text">
+            {" "}
+            Navbar text with an inline element{" "}
+          </span>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+};
