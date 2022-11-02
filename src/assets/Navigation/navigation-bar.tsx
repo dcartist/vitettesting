ccaa68703749fa26692f6b12bcb22f2aa3274880
dcartist <pbannerman13@gmail.com>
2022-11-01 23:49:28 -0400

    updating navigation

--- src/assets/Navigation/navigation-bar.tsx
+++ src/assets/Navigation/navigation-bar.tsx
@@ -15,39 +15,47 @@ export const NavBar = () => {
   const [showNavText, setShowNavText] = useState(false);
 
   return (
-    <MDBNavbar expand="lg" light bgColor="light">
-      <MDBContainer fluid>
-        <MDBNavbarBrand href="#">FlickPix</MDBNavbarBrand>
-        <MDBNavbarToggler
-          type="button"
-          data-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={() => setShowNavText(!showNavText)}
-        >
-          <MDBIcon icon="bars" fas />
-        </MDBNavbarToggler>
-        <MDBCollapse navbar show={showNavText}>
-          <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current="page" href="#">
-                Home
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href="#">Features</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href="#">Pricing</MDBNavbarLink>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-          <span className="navbar-text">
-            {" "}
-            Navbar text with an inline element{" "}
-          </span>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+    <>
+      <MDBNavbar expand="lg" light bgColor="light">
+        <MDBContainer fluid>
+          <MDBNavbarBrand href="#">FlickPix</MDBNavbarBrand>
+          <MDBNavbarToggler
+            type="button"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setShowNavText(!showNavText)}
+          >
+            <MDBIcon icon="bars" fas />
+          </MDBNavbarToggler>
+          <MDBCollapse navbar show={showNavText}>
+            <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current="page" href="#">
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">Features</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">Pricing</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+            <MDBNavbarLink href="#">
+              <MDBIcon fas icon="shopping-cart" />
+            </MDBNavbarLink>
+            <MDBNavbarLink href="#">
+              <MDBIcon fab icon="twitter" />
+            </MDBNavbarLink>
+            {/* <span className="navbar-text">
+              {" "}
+              Navbar text with an inline element{" "}
+            </span> */}
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
   );
 };
